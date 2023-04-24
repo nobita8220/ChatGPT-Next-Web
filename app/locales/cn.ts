715a1ccfd323672ba25e2c28561c1291d8e0d578
nobita8220 <63402410+nobita8220@users.noreply.github.com>
2023-04-24 21:56:11 +0800

    Update cn.ts

--- app/locales/cn.ts
+++ app/locales/cn.ts
@@ -3,7 +3,7 @@ import { SubmitKey } from "../store/config";
 const cn = {
   WIP: "该功能仍在开发中……",
   Error: {
-    Unauthorized: `当前访问未授权，请联系微信 \ `Deuxsept-NGAI`,备注` \ chatGPT授权 \ `![ddwechat](/wechat.jpg)`,
+    Unauthorized: "现在是未授权状态，请点击左下角设置按钮输入访问密码。",
   },
   ChatItem: {
     ChatItemCount: (count: number) => `${count} 条对话`,
